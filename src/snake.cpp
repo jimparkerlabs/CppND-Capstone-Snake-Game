7254b868629cb0c1c28c63f5c631c6f8faec90be
Jim Parker <jim@jimparkertlabs.com>
2024-09-09 16:36:23 -0500

    I think I finished GameObject class.
    TODO:
      snake energy
      threads
      controller should control food and other snakes
      bounce, not wrap

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,19 +1,20 @@
 #include "snake.h"
 #include <cmath>
 #include <iostream>
+#include "WorldObject.h"
 
 void Snake::Update() {
     UpdateHead();
     UpdateBody();
 
     // wrap segments if necessary
-    x = fmod(x + grid_width, grid_width);
-    y = fmod(y + grid_height, grid_height);
+//    x = fmod(x + grid_width, grid_width);
+//    y = fmod(y + grid_height, grid_height);
 
     // iterate over body and do the same
     for (Snake::coordinate &point : body) {
-        point.x = fmod(point.x + grid_width, grid_width);
-        point.y = fmod(point.y + grid_height, grid_height);
+//        point.x = fmod(point.x + grid_width, grid_width);
+//        point.y = fmod(point.y + grid_height, grid_height);
     }
 }
 
@@ -24,10 +25,10 @@ void Snake::UpdateHead() {
 void Snake::UpdateBody() {
     if (growing) {
         if (body.empty()) {
-            addBodySegment(coordinate{x, y}, heading.x, heading.y);
+            addBodySegment(_position, _heading);
             growing = false;
         } else if (body.size() == 1){
-            addBodySegment(body[0], coordinate {x, y});
+            addBodySegment(body[0], _position);
             growing = false;
         } else {
             addBodySegment(body.back(), body[body.size() - 2]);
@@ -37,7 +38,7 @@ void Snake::UpdateBody() {
 
     if (!body.empty()) {
         // move first body segment
-        body[0] = moveBody(body[0], Snake::coordinate{x, y});
+        body[0] = moveBody(body[0], _position);
     }
 
     for (std::size_t i = 1; i < body.size(); ++i) {
@@ -47,7 +48,7 @@ void Snake::UpdateBody() {
     // Check if the snake has died
     // TODO: change this to cut the snake
     for (auto const &item : body) {
-        if (x == item.x && y == item.y) {
+        if (_position.x == item.x && _position.y == item.y) {
             alive = false;
             std::cout << "You are dead!" << std::endl;
             alive = true;
@@ -73,15 +74,14 @@ void Snake::GrowBody() { growing = true; }
 
 // Inefficient method to check if cell is occupied by snake.
 bool Snake::isOccupying(const coordinate &point) const {
-  if (point.x == x && point.y == y) {
-    return true;
-  }
-  for (auto const &item : body) {
-    if (point.x == item.x && point.y == item.y) {
-      return true;
+    if (WorldObject::isOccupying(point)) return true;
+
+    for (auto const &item : body) {
+        if (point.x == item.x && point.y == item.y) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 //void Snake::move() {
